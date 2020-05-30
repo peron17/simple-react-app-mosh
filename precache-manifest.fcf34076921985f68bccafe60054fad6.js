84a8cc6d851fa4ec7ded9448ebd96f39273b89ff
Peron <tommypriambodo@gmail.com>
2020-05-31 00:41:24 +0700

    Updates

---
+++ precache-manifest.fcf34076921985f68bccafe60054fad6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "395fc06f9fa315d6b66dce59ac59b3d6",
+    "url": "/simple-react-app-mosh/index.html"
+  },
+  {
+    "revision": "c393987089866c4a63b9",
+    "url": "/simple-react-app-mosh/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c393987089866c4a63b9",
+    "url": "/simple-react-app-mosh/static/js/2.89d60b34.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-react-app-mosh/static/js/2.89d60b34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7398cd2b2f3392747012",
+    "url": "/simple-react-app-mosh/static/js/main.59f9bdec.chunk.js"
+  },
+  {
+    "revision": "532c2fae7b1cdca17dfa",
+    "url": "/simple-react-app-mosh/static/js/runtime-main.21628eb5.js"
+  }
+]);
